b1f87764b579dfeaa9b5065ac194aa909d48f33a
vishek-patel <vishek.12001921@lpu.in>
2023-03-23 10:58:02 +0530

    added all the files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,38 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'randomQuote';
+  quote:string = "----Random quote comes here...---"
+  author:string="---author name---"
+  API_KEY = "caaa0681fae72d1818249bc9511fabc8"
+  location:string = "new delhi"
+
+  constructor(private http:HttpClient){
+    
+  }
+
+  getQuote(){
+
+    //open weather app api
+
+    // this.http.get(`https://api.openweathermap.org/data/2.5/weather?q=${this.location}&appid=${this.API_KEY}`).subscribe((data:any)=>{
+    //     console.log(data);
+    //     console.log(data.main.temp)
+    //     console.log(data.main.feels_like)
+    //     console.log(data.main.humidity)
+    //     console.log(data.main.pressure)
+    //     console.log(data.weather[0].description)
+
+    //    })
+
+
+   
+
+  // quote api call method
+  this.http.get('https://api.quotable.io/random').subscribe((data:any)=>{
+    this.quote = data.content;
+    this.author = data.author;
+  })
+
+
+  }
 }
